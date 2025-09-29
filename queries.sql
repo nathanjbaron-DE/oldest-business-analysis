51f2834ae1cd9ba5e7bd24cfc158c41eefe7aebb
nathanjbaron-DE <nathanjbaron@gmail.com>
2025-09-28 19:12:08 -0500

    Update queries.sql
    Signed-off-by: nathanjbaron-DE <nathanjbaron@gmail.com>

--- queries.sql
+++ queries.sql
@@ -1,18 +1,19 @@
--- Save oldest business per continent
 .headers on
 .mode csv
+
+-- Oldest business per continent
 .output oldest_businesses.csv
 SELECT c.continent, c.country, b.business, b.year_founded
 FROM countries c
 JOIN businesses b ON c.country_code = b.country_code
 WHERE b.year_founded = (
-SELECT MIN(b2.year_founded)
-FROM businesses b2
-JOIN countries c2 ON b2.country_code = c2.country_code
-WHERE c2.continent = c.continent
+    SELECT MIN(b2.year_founded)
+    FROM businesses b2
+    JOIN countries c2 ON b2.country_code = c2.country_code
+    WHERE c2.continent = c.continent
 );
 
--- Save countries missing business data
+-- Countries missing business data
 .output countries_missing_businesses.csv
 SELECT c.continent, COUNT(c.country) AS countries_without_businesses
 FROM countries c
@@ -20,7 +21,7 @@ LEFT JOIN businesses b ON c.country_code = b.country_code
 WHERE b.business IS NULL
 GROUP BY c.continent;
 
--- Save longest lasting business categories
+-- Longest lasting categories
 .output longest_lasting_categories.csv
 SELECT c.continent, cat.category, MIN(b.year_founded) AS year_founded
 FROM countries c
